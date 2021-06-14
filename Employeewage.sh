04db22e15f0e69b6285416e64cbedfcd96b6458b
lavanya-pg <lavanyapg8@gmail.com>
2021-06-14 20:23:09 +0530

    using random fumction

--- Employeewage.sh
+++ Employeewage.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo "welcome to employee wage computation program"
+ispresent=1
+randomcheck=$((RANDOM%2))
+if [ $ispresent -eq $randomcheck ]
+then
+	echo "employee is present"
+else
+	echo "employee is absent"
+fi
