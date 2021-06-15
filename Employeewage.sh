a68bb7a49e42188bc3ba832a40dc3e7e976037d1
lavanya-pg <lavanyapg8@gmail.com>
2021-06-15 10:49:39 +0530

    assuming fullworking hour and wage per hour

--- Employeewage.sh
+++ Employeewage.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 echo "welcome to employee wage computation program"
 ispresent=1
+employee_workinghourperday=8
+employeerate_perhour=20
 randomcheck=$((RANDOM%2))
 if [ $ispresent -eq $randomcheck ]
 then
