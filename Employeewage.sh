5f25c5bfefb8e12f5fd5a234e2a33ef70dda0c8a
lavanya-pg <lavanyapg8@gmail.com>
2021-06-15 11:04:22 +0530

    checking for employee is parttime or fulltime present

--- Employeewage.sh
+++ Employeewage.sh
@@ -3,10 +3,13 @@ echo "welcome to employee wage computation program"
 ispresent=1
 employee_workinghourperday=8
 employeerate_perhour=20
-randomcheck=$((RANDOM%2))
+randomcheck=$((RANDOM%3))
 if [ $ispresent -eq $randomcheck ]
 then
 	echo "employee is present"
+elif [ $randomcheck -gt $ispresent ]
+then
+	echo "employee is part-time present"
 else
 	echo "employee is absent"
 fi
